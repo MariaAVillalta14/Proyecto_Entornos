a50a10adcc9943d8add23f030f704a0fd7d7902f
MariaAVillalta14 <villaltamaria14@gmail.com>
2020-04-26 09:52:05 +0200

    subida

---
+++ CODIGO/MKM_DEVELOPMENT_PROYECTO_1/BIBLIO_MOVIL/src/main/java/Dominio/Sancion.java
@@ -0,0 +1,121 @@
+package Dominio;
+
+import java.util.*;
+
+public class Sancion {
+
+	private int Numero_dias_sancion;
+	private String emailSocio;
+	private String titulo_libro_prestamo;
+	ArrayList <Sancion> mis_sanciones;
+
+	public Sancion(String emailSocio,String titulo_libro_prestamo) 
+	{
+		this.Numero_dias_sancion = 10;
+		this.emailSocio = emailSocio;
+		this.titulo_libro_prestamo = titulo_libro_prestamo;
+	}
+	
+	public Sancion()
+	{
+		this.mis_sanciones = new ArrayList<Sancion>();
+		mis_sanciones.add(new Sancion("zapatacamila@gmail.com","Cien años de soledad"));
+	}
+
+	public int getNumero_dias_sancion() {
+		return Numero_dias_sancion;
+	}
+
+	public void setNumero_dias_sancion(int numero_dias_sancion) {
+		Numero_dias_sancion = numero_dias_sancion;
+	}
+	
+	
+	public int setFecha(Calendar fechaFinPrestamo, int dia)
+	{
+		Calendar fechaActual = Calendar.getInstance();
+		fechaActual.add(Calendar.DAY_OF_MONTH, dia);
+		return Numero_dias_sancion;
+	}
+	
+		
+	public String getEmailSocio() {
+		return emailSocio;
+	}
+
+	public void setEmailSocio(String emailSocio) {
+		this.emailSocio = emailSocio;
+	}
+
+	public String getTitulo_libro_prestamo() {
+		return titulo_libro_prestamo;
+	}
+
+	public void setTitulo_libro_prestamo(String titulo_libro_prestamo) {
+		this.titulo_libro_prestamo = titulo_libro_prestamo;
+	}
+
+	public void anyadirSancion(Sancion misancion)
+	{	
+		mis_sanciones.add(misancion);
+	}
+
+	public Sancion miSancion()
+	{ 
+		Sancion sancion = null;
+		for(int i=0; i<mis_sanciones.size();i++) 
+		{
+			sancion=mis_sanciones.get(i);
+		}
+		return sancion;
+	}
+
+	//prueba
+	public void verSancion1(String email)
+	{		
+		System.out.println("*********** SANCIONES ***********");
+			for(int i=0; i<mis_sanciones.size();i++) 
+			{
+				if(mis_sanciones.get(i).getEmailSocio().equalsIgnoreCase(email))
+				{
+					
+					System.out.println("Tienes una sanción de 10 dias por el libro " + mis_sanciones.get(i).getTitulo_libro_prestamo());
+					System.out.println("No puedes alquilar otro libro hasta que finalice la sanción");
+				}
+				else
+				{
+					System.out.println("No tiene penalizaciones");
+				}
+			}
+					
+	}
+	
+	public boolean comprobarSancion(String email)
+	{		boolean sancion=false;
+			for(int i=0; i<mis_sanciones.size();i++) 
+			{
+				if(mis_sanciones.get(i).getEmailSocio().equalsIgnoreCase(email))
+				{
+					sancion=true;
+				}
+			}
+			
+			return sancion; 
+	}				
+	
+	
+	
+	public void verSancion(String email)
+	{System.out.println("*********** SANCIONES ***********");
+		if(this.comprobarSancion(email) ==true)
+		{
+			System.out.println("Tienes una sanción de 10 dias por el libro " + this.miSancion().getTitulo_libro_prestamo());
+			System.out.println("No puedes alquilar otro libro hasta que finalice la sanción");
+		}
+		else
+		{
+			System.out.println("No tiene penalizaciones");
+		}
+	}
+
+}
---
+++ CODIGO/MKM_DEVELOPMENT_PROYECTO_1/BIBLIO_MOVIL/src/main/java/Dominio/Socio.java
@@ -0,0 +1,83 @@
+package Dominio;
+import java.util.*;
+
+public class Socio extends Usuario {
+	
+	static BiblioMovil mi_socio = new BiblioMovil();
+	static Scanner entrada = new Scanner(System.in);
+	
+	private String nombre;
+	private String apellidos;
+	private String fecha_nacimiento;
+	private String email;
+	private String contrasenya;
+	
+	
+	public Socio(String nombre, String apellidos, String fecha_nacimiento, String email,String contrasenya) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.fecha_nacimiento = fecha_nacimiento;
+		this.email = email;
+		this.contrasenya=contrasenya;
+	}
+	
+	public Socio(String email)
+	{
+		this.email=email;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+
+	public String getFecha_nacimiento() {
+		return fecha_nacimiento;
+	}
+
+
+	public void setFecha_nacimiento(String fecha_nacimiento) {
+		this.fecha_nacimiento = fecha_nacimiento;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getContrasenya() {
+		return contrasenya;
+	}
+
+
+	public void setContrasenya(String contrasenya) {
+		this.contrasenya = contrasenya;
+	}
+	
+	
+	
+	
+}
