cabcb883a0e05c2304d89f6e97be7097ee133f3d
MariaAVillalta14 <villaltamaria14@gmail.com>
2020-04-26 19:00:25 +0200

    Subida

---
+++ CODIGO/MKM_DEVELOPMENT_PROYECTO_1/BIBLIO_MOVIL/BIBLIO_MOVIL_VERSION_1.4/src/main/java/Presentacion/ClasePrincipal.java
@@ -0,0 +1,390 @@
+package Presentacion;
+
+import java.util.*;
+
+import Dominio.BiblioMovil;
+import Dominio.Prestamo;
+import Dominio.Sancion;
+import Dominio.Usuario;
+
+public class ClasePrincipal {
+	
+	static Scanner entrada=new Scanner(System.in);
+	static BiblioMovil librosBiblio=new BiblioMovil();
+	static BiblioMovil mi_socio = new BiblioMovil();
+	static Sancion sancion=new Sancion();
+	static boolean continuar=true;
+	static boolean continuar2=false;
+	static boolean corresponde=true;
+	static String titulo;
+	static Prestamo miPrestamo=new Prestamo();
+	static String email;
+	
+	public static void main(String[] args) {
+		
+
+		menu();
+		
+
+	}
+	
+	public static void menu()
+	{
+		int opcion=0;
+		do
+		{
+			System.out.println("****MENU PRINCIPAL*****");
+			System.out.println("1.Ver catalogo");
+			System.out.println("2.Acceder");
+			System.out.println("3.Registrarse");
+			System.out.println("4.Salir");
+			System.out.println("Introduzca una opción: ");
+			
+			opcion=entrada.nextInt();
+			
+			switch(opcion)
+			{
+			case(1):					
+			    librosBiblio.mostrarCatalago();
+				mostrarDescripcionLibro();    
+				break;
+			case(2):	
+				accesoSocio();
+		        continuar2=true;
+				break;
+			case(3):
+				Usuario miusuario=new Usuario();
+			    mi_socio.registrarSocio(miusuario.Registrarse(mi_socio));
+			    continuar2=true;
+				break;
+			case(4):
+				System.out.println("Ha salido del programa");
+			    continuar2=false;
+				break;
+			default:
+				System.out.println("Opción errónea, introduzca una opción válida");
+			}
+		}while(opcion <1 && opcion >4 || continuar2==true);
+		
+	}
+	
+	public static void mostrarDescripcionLibro()
+	{
+		 System.out.println("¿Quiere ver la descripción de algún libro? SI/NO");
+		    String resp=entrada.nextLine();
+		    
+		    if(resp.isEmpty())
+		    {
+		    	resp=entrada.nextLine();
+		    }
+        
+		    do
+         {
+         	if(resp.equalsIgnoreCase("SI"))
+ 		    {
+
+         			System.out.println("Escriba el titulo del libro:");
+     		    	   	
+                    
+     		    	do
+     		    	{
+     		    		String titulo=entrada.nextLine();
+     		    		if(titulo.isEmpty())
+	        		    	{
+	        		    		titulo=entrada.nextLine();
+	        		    	}
+     		    	    if(librosBiblio.tituloLibro(titulo).equalsIgnoreCase(titulo))
+	        		    	{
+     		    	    	System.out.println(librosBiblio.descripcionLibro(titulo));
+     		    	    	corresponde=false;
+     		    	    	System.out.println("¿Quiere ver la descripción de otro libro? SI/NO");
+	        		    		
+     		    	    	String resp2=entrada.nextLine();
+		        		    	if(resp2.isEmpty())
+		        		    	{
+		        		    		resp2=entrada.nextLine();
+		        		    	}
+		        		    	if(resp2.equalsIgnoreCase("NO"))
+		        		    	{
+		        		    		continuar=false;
+		        		    		continuar2=true;
+		        		    	}
+		        		        if(resp2.equalsIgnoreCase("SI"))
+		        		    	{
+		        		        	
+                                continuar=true;
+		        		    	}
+	        		    		
+	        		    	}
+     		    		else
+	        		    	{
+	        		    		System.out.println("El titulo del libro no corresponde a ninguno de nuestra lista, ingrese de nuevo el titulo: ");
+	        		    		corresponde=true;
+	        		    	}
+	        		    	
+     		    	}while(corresponde==true);
+     		    	
+    
+ 		    }
+ 		    if(resp.equalsIgnoreCase("NO"))
+ 		    {
+ 		    	continuar=false;
+ 		    	continuar2=true;
+ 		    }
+         }while(continuar==true);
+	}
+	
+	public static void accesoSocio()
+	{
+		boolean correcto=false;
+		boolean correcto2=false;
+		System.out.println("***ACCEDER***");
+		    System.out.println("1.Usuario"); 
+		    
+		     do
+		     {
+		    	 email = entrada.nextLine();
+				    if(email.isEmpty())
+				    {
+				    	email = entrada.nextLine();
+				    }
+			 
+				    if(mi_socio.Comprobar_email(email).equalsIgnoreCase(email))
+				    {
+				    	correcto=true;
+					    System.out.println("2.Contraseña"); 
+					    do
+					    {
+					    	String contrasenya = entrada.nextLine();
+							if(contrasenya.isEmpty())
+							{
+								contrasenya = entrada.nextLine();
+							}	
+							if(mi_socio.Comprobar_contrasenya(contrasenya).equalsIgnoreCase(contrasenya))
+							{
+								correcto=true;
+								menuSocio();
+								
+							}
+							else
+							{
+								System.out.println("Contraseña incorrecta, intente de nuevo: ");
+								correcto2=false;
+							}
+					    }while(correcto2==false);
+						
+				    }
+				    else
+				    {
+				    	System.out.println("Email incorrecto, ingrese de nuevo su email: ");
+				    	correcto=false;
+				    } 
+		     }while(correcto==false);
+	}
+
+	public static void menuSocio()
+	{
+		int opcion2=0;
+		boolean continuar3=true;
+		do
+		{
+			System.out.println("*****MENÚ SOCIO******");
+		    System.out.println("1.Ver catalogo");
+		    System.out.println("2.Alquileres");
+		    System.out.println("3.Sanciones");
+		    System.out.println("4.Salir");
+		     
+		    System.out.println("Introduzca una opcion:");
+		    opcion2=entrada.nextInt();
+	
+		    
+		    switch(opcion2)
+		    {
+		    case(1):
+		    	librosBiblio.mostrarDisponibilidad();
+		        mostrarDescripcionLibroSocios();
+		    	break;
+		    case(2):
+		    	miPrestamo.verPrestamos(email);
+		        devolverLibro();
+		    	break;
+		    case(3):
+		    	sancion.verSancion(email);
+		    	break;
+		    case(4):
+		    	menu();
+		    	continuar2=true;
+		    	continuar3=false;  
+		    	break;
+		    default:
+		    	System.out.println("Opción errónea, intente de nuevo");
+		    	break;
+		    }
+		}while(opcion2 <1 && opcion2>4 ||continuar3==true);
+	     
+	}
+
+	public static void mostrarDescripcionLibroSocios()
+	{
+		System.out.println("¿Quiere ver la descripción de algún libro? SI/NO");
+	    String resp=entrada.nextLine();
+	    
+	    if(resp.isEmpty())
+	    {
+	    	resp=entrada.nextLine();
+	    }
+    
+	    do
+	    {
+     	if(resp.equalsIgnoreCase("SI"))
+		    {
+
+     			System.out.println("Escriba el titulo del libro:");
+ 		    	   	
+                
+ 		    	do
+ 		    	{
+ 		    		titulo=entrada.nextLine();
+ 		    		if(titulo.isEmpty())
+        		    	{
+        		    		titulo=entrada.nextLine();
+        		    	}
+ 		    	    if(librosBiblio.tituloLibro(titulo).equalsIgnoreCase(titulo))
+        		    	{
+ 		    	    	System.out.println(librosBiblio.descripcionLibro(titulo));
+ 		    	    	 alquilarLibro();	
+ 		    	    	corresponde=false;
+ 		    	    	System.out.println("¿Quiere ver la descripción de otro libro? SI/NO");
+        		    		
+ 		    	    	String resp2=entrada.nextLine();
+	        		    	if(resp2.isEmpty())
+	        		    	{
+	        		    		resp2=entrada.nextLine();
+	        		    	}
+	        		    	if(resp2.equalsIgnoreCase("NO"))
+	        		    	{
+	        		    		continuar=false;
+	        		    		continuar2=true;
+	        		    	}
+	        		        if(resp2.equalsIgnoreCase("SI"))
+	        		    	{
+	        		       
+                            continuar=true;
+	        		    	}
+        		    		
+        		    	}
+ 		    		else
+        		    	{
+        		    		System.out.println("El titulo del libro no corresponde a ninguno de nuestra lista, ingrese de nuevo el titulo: ");
+        		    		corresponde=true;
+        		    	}
+        		    	
+ 		    	}while(corresponde==true);
+ 		    	
+
+		    }
+		    if(resp.equalsIgnoreCase("NO"))
+		    {
+		    	continuar=false;
+		    	continuar2=true;
+		    }
+     }while(continuar==true);
+	}
+	
+	public static void alquilarLibro()
+	{
+		
+		Calendar fecha=GregorianCalendar.getInstance();
+		int anyo = fecha.get(Calendar.YEAR);
+		int mes = fecha.get(Calendar.MONTH);
+		int dia = fecha.get(Calendar.DAY_OF_MONTH);
+		fecha.set(Calendar.SECOND,0);
+		fecha.set(Calendar.MILLISECOND, 0);
+		
+		System.out.println("¿Desea alquilar este libro? SI/NO");
+		String resp3=entrada.nextLine();
+		if(resp3.isEmpty())
+		{
+			resp3=entrada.nextLine();
+		}
+
+		if(resp3.equalsIgnoreCase("SI"))
+		{
+			if(sancion.comprobarSancion(email)==false)
+			{
+				if(librosBiblio.getDisponibilidad(titulo).equalsIgnoreCase("Disponible"))
+				{
+					miPrestamo.anyadirPrestamo(new Prestamo(Prestamo.fecha_inicio(anyo,mes,dia ),Prestamo.fecha_fin(anyo,mes,dia),email,librosBiblio.Libro(titulo).getTitulo()));
+				}
+				else
+				{
+					System.out.println("Este libro no se encuentra disponible, no lo puede alquilar");
+				}
+			}
+			else
+			{
+				System.out.println("Usted tiene una sanción, no puedes alquilar otro libro hasta que finalice la sanción");
+			}
+		}
+		
+	}
+	
+	public static void devolverLibro()
+	{
+		
+		boolean repetir=false;
+		System.out.println("\n¿Quiere devolver alguno de los libros que tiene en prestamo? SI/NO");
+		String resp4=entrada.nextLine();
+		if(resp4.isEmpty())
+		{
+			resp4=entrada.nextLine();
+		}
+		if(resp4.equalsIgnoreCase("SI"))
+		{
+			System.out.println("Introduzca el titulo del libro que desea devolver: ");
+			do
+			{
+				String tituloNombreADevolver=entrada.nextLine();
+				if(tituloNombreADevolver.isEmpty())
+				{
+					tituloNombreADevolver=entrada.nextLine();
+				}
+
+				
+				if(miPrestamo.tituloCoincide(email,tituloNombreADevolver)==true)
+				{
+					
+					miPrestamo.devolverLibro(email,librosBiblio.Libro(tituloNombreADevolver).getTitulo());
+					
+					Calendar fecha2=Calendar.getInstance();
+					fecha2.set(Calendar.SECOND,0);
+					fecha2.set(Calendar.MILLISECOND, 0);
+					
+					if(miPrestamo.obtenerFechaPrestamo(email, titulo).after(fecha2) == true)
+					{
+						sancion.anyadirSancion(new Sancion(email,tituloNombreADevolver));
+					}
+					repetir=false;
+				
+				}
+				else
+				{
+					System.out.println("No tiene un prestamo de libro con el titulo:  '" +tituloNombreADevolver + "' , por favor, ingrese de nuevo el titulo del libro que desea devolver:");
+					repetir=true;
+				}
+			}while(repetir==true);
+			
+			
+			
+			
+				
+			
+			
+			
+			
+			
+		}
+	}
+
+	
+}
---
+++ CODIGO/MKM_DEVELOPMENT_PROYECTO_1/BIBLIO_MOVIL/BIBLIO_MOVIL_VERSION_1.4/src/test/java/Dominio/PrestamoTest.java
@@ -0,0 +1,249 @@
+package Dominio;
+
+import static org.junit.Assert.*;
+
+
+import java.util.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import Dominio.Prestamo;
+
+public class PrestamoTest {
+	
+	static Prestamo librosPrestados;
+	static Prestamo librosPrestados2;
+	static ArrayList<Prestamo> librosEnPrestamo;
+	
+	@BeforeClass
+	public static void beforeClass() 
+	{
+	 	System.out.println("Estoy metodo beforeClass");
+	 	librosPrestados=new Prestamo();
+	 	librosEnPrestamo=new ArrayList<Prestamo>();
+	 	librosEnPrestamo.add(new Prestamo(Prestamo.fecha_inicio(2020, Calendar.APRIL, 1),Prestamo.fecha_fin(2020,Calendar.APRIL,1),"villaltamaria@gmail.com","El Alquimista"));
+		librosEnPrestamo.add(new Prestamo(Prestamo.fecha_inicio(2020, Calendar.APRIL, 2),Prestamo.fecha_fin(2020,Calendar.APRIL,2),"villaltamaria@gmail.com","Cien años de soledad"));
+		librosEnPrestamo.add(new Prestamo(Prestamo.fecha_inicio(2020, Calendar.APRIL, 3),Prestamo.fecha_fin(2020,Calendar.APRIL,3),"zapatacamila@gmail.com","Cien años de soledad"));
+		librosEnPrestamo.add(new Prestamo(Prestamo.fecha_inicio(2020, Calendar.JANUARY, 4),Prestamo.fecha_fin(2020,Calendar.JANUARY,4),"chaconkevim@gmail.com","La chica de nieve"));	
+	 	librosEnPrestamo.add(new Prestamo(Prestamo.fecha_inicio(2020, Calendar.JANUARY, 4),Prestamo.fecha_fin(2020,Calendar.JANUARY,4),"anto123@gmail.com","El Alquimista"));
+	 	librosPrestados2=new Prestamo();
+	 	
+	}
+
+	@Before
+	public void before() 
+	{
+		System.out.println("Estoy metodo before");
+	}
+			
+
+	//Método --> registrarSocio(Socio misocio)
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("Estoy en método getSocio");	
+		ArrayList<Prestamo> esperado=librosEnPrestamo;
+			
+		Prestamo nuevoPrestamo=new Prestamo(Prestamo.fecha_inicio(2020, Calendar.JANUARY, 4),Prestamo.fecha_fin(2020,Calendar.JANUARY,4),"anto123@gmail.com","El Alquimista");
+		librosPrestados.anyadirPrestamo(nuevoPrestamo);
+		
+		ArrayList<Prestamo> resultado=librosPrestados.getLibrosPrestados();
+		
+		int i=0;
+		boolean encontrado=true;
+		int maximo=0;
+			
+		if(esperado.size()>resultado.size()) 
+		{
+			maximo=esperado.size();
+		}
+			
+		else 
+		{
+			maximo=resultado.size();
+			System.out.println("Se ha añadido un prestamo");
+		}
+			
+		while(i<maximo) 
+		{
+			try
+			{
+				if(!esperado.get(i).toString().equals(resultado.get(i).toString())) 
+				{
+					encontrado=false;
+					System.out.println(esperado.get(i).toString());
+					System.out.println(resultado.get(i).toString());
+				}
+				}
+				catch(IndexOutOfBoundsException e) 
+				{
+					System.out.println("Una de las listas tiene más elementos, no ha añadido el Prestamo");
+					encontrado=false;
+				}
+
+				i++;
+			}
+			assertTrue(encontrado);
+			
+		}	 	
+
+	//Método --> tituloCoincide(String email,String titulo)
+	@Test
+	public void test2()
+	{
+		System.out.println("Estoy en metodo test tituloCoincide(String email,String titulo) cuando existen email y titulo");
+		boolean esperado=true;
+		boolean resultado=librosPrestados.tituloCoincide("villaltamaria@gmail.com", "Cien años de soledad");
+		assertEquals(esperado,resultado);
+	}
+	
+	@Test
+	public void test3()
+	{
+		System.out.println("Estoy en metodo test tituloCoincide(String email,String titulo) cuando existen email pero NO titulo");
+		boolean esperado=false;
+		boolean resultado=librosPrestados.tituloCoincide("villaltamaria@gmail.com", "La chica de nieve");
+		assertEquals(esperado,resultado);
+	}
+	
+	@Test
+	public void test4()
+	{
+		System.out.println("Estoy en metodo test tituloCoincide(String email,String titulo) cuando NO existe email y SI titulo");
+		boolean esperado=false;
+		boolean resultado=librosPrestados.tituloCoincide("estemailnoexiste@gmail.com", "La chica de nieve");
+		assertEquals(esperado,resultado);
+	}
+	
+	@Test
+	public void test5()
+	{
+		System.out.println("Estoy en metodo test tituloCoincide(String email,String titulo) cuando existe email y NO titulo");
+		boolean esperado=false;
+		boolean resultado=librosPrestados.tituloCoincide("villaltamaria14@gmail.com", "Este libro no esta en prestamo");
+		assertEquals(esperado,resultado);
+	}
+	
+	@Test
+	public void test6()
+	{
+		System.out.println("Estoy en metodo test tituloCoincide(String email,String titulo) cuando NO existen email ni titulo");
+		boolean esperado=false;
+		boolean resultado=librosPrestados.tituloCoincide("estemailnoexiste@gmail.com", "Este libro no esta en prestamo");
+		assertEquals(esperado,resultado);
+	}
+	
+	//Método --> obtenerFechaPrestamo(String emailSocio,String titulo)
+	
+	@Test
+	public void test7()
+	{
+		System.out.println("Estoy en metodo test obtenerFechaPrestamo(String emailSocio,String titulo) cuando existe email y titulo");
+		
+		Calendar esperado=(Prestamo.fecha_fin(2020,Calendar.APRIL,2));
+		Calendar resultado=librosPrestados.obtenerFechaPrestamo("villaltamaria@gmail.com", "Cien años de soledad");
+
+		assertEquals(esperado,resultado);
+	}
+	
+	@Test
+	public void test8()
+	{
+		System.out.println("Estoy en metodo test obtenerFechaPrestamo(String emailSocio,String titulo) cuando NO coincide email y ni titulo");
+		
+		Calendar esperado=Calendar.getInstance();
+		esperado.set(Calendar.MINUTE,0);
+ 		esperado.set(Calendar.MILLISECOND,0);
+		Calendar resultado=librosPrestados.obtenerFechaPrestamo("estemailnoexiste@gmail.com.", "Cien años de soledad");
+		
+		assertEquals(esperado,resultado);
+	}
+	
+	@Test
+	public void test9()
+	{
+		System.out.println("Estoy en metodo test obtenerFechaPrestamo(String emailSocio,String titulo) cuando SI coincide email y NO titulo");
+		
+		Calendar esperado=Calendar.getInstance();
+		esperado.set(Calendar.MINUTE,0);
+ 		esperado.set(Calendar.MILLISECOND,0);
+		Calendar resultado=librosPrestados.obtenerFechaPrestamo("villaltamaria@gmail.com", "Lugares asombrosos");
+		
+		assertEquals(esperado,resultado);
+	}
+	
+	//Método --> devolverLibro(String emailSocio,String titulo)
+	@Test
+	public void test10()
+	{
+		System.out.println("Estoy en método devolverLibro(String emailSocio,String titulo)");	
+		ArrayList<Prestamo> esperado=librosEnPrestamo;
+			
+		Prestamo nuevoPrestamo=new Prestamo(Prestamo.fecha_inicio(2020, Calendar.JANUARY, 4),Prestamo.fecha_fin(2020,Calendar.JANUARY,4),"anto123@gmail.com","El Alquimista");
+		Prestamo nuevoPrestamoParaDevolver=new Prestamo(Prestamo.fecha_inicio(2020, Calendar.JANUARY, 4),Prestamo.fecha_fin(2020,Calendar.JANUARY,4),"jonh612@outlook.com","El Alquimista");
+		librosPrestados2.anyadirPrestamo(nuevoPrestamo);
+		librosPrestados2.anyadirPrestamo(nuevoPrestamoParaDevolver);
+		librosPrestados2.devolverLibro("jonh612@outlook.com","El Alquimista");
+		
+		ArrayList<Prestamo> resultado=librosPrestados2.getLibrosPrestados();
+		
+		int i=0;
+		boolean encontrado=true;
+		int maximo=0;
+			
+		if(esperado.size()>resultado.size()) 
+		{
+			maximo=esperado.size();
+		}
+			
+		else 
+		{
+			maximo=resultado.size();
+			System.out.println("Se ha añadido un prestamo");
+		}
+			
+		while(i<maximo) 
+		{
+			try
+			{
+				if(!esperado.get(i).toString().equals(resultado.get(i).toString())) 
+				{
+					encontrado=false;
+					System.out.println(esperado.get(i).toString());
+					System.out.println(resultado.get(i).toString());
+				}
+				}
+				catch(IndexOutOfBoundsException e) 
+				{
+					System.out.println("Una de las listas tiene más elementos, no ha añadido el Prestamo");
+					encontrado=false;
+				}
+
+				i++;
+			}
+			assertTrue(encontrado);
+	}
+	
+	
+	@After
+	public void after() 
+	{
+		System.out.println("Estoy metodo after"); 
+		
+	}
+		 			
+	@AfterClass 
+	public static void afterClass() 
+	{
+		System.out.println("Estoy metodo afterClass"); 	
+	}	
+		
+		
+	}
+
+
