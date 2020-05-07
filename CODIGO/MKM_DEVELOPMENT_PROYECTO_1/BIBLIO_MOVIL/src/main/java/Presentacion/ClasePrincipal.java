f5f8849983bdb51dfc20cedeee98d4b448cee49c
MariaAVillalta14 <villaltamaria14@gmail.com>
2020-05-07 23:00:21 +0200

    Subida

--- CODIGO/MKM_DEVELOPMENT_PROYECTO_1/BIBLIO_MOVIL/src/main/java/Presentacion/ClasePrincipal.java
+++ CODIGO/MKM_DEVELOPMENT_PROYECTO_1/BIBLIO_MOVIL/src/main/java/Presentacion/ClasePrincipal.java
@@ -1,5 +1,7 @@
 package Presentacion;
 
+
+import java.text.*;
 import java.util.*;
 
 import Dominio.BiblioMovil;
@@ -30,209 +32,223 @@ public class ClasePrincipal {
 	public static void menu() 
 	{
 		int opcion=0;
-		
-			try 
-			{	
-				
-				do
-				{
-					
-					System.out.println("****MENU PRINCIPAL*****");
-					System.out.println("1.Ver catalogo");
-					System.out.println("2.Acceder");
-					System.out.println("3.Registrarse");
-					System.out.println("4.Salir");
-					System.out.println("Introduzca una opción: ");
-				
-						opcion=entrada.nextInt();
-						
-				switch(opcion)
-				{
-				
-				case(1):					
-				    librosBiblio.mostrarCatalago();
-					mostrarDescripcionLibro();    
-					break;
-				case(2):	
-					accesoSocio();
-			        continuar2=true;
-					break;
-				case(3):
-					Usuario miusuario=new Usuario();
-
-				System.out.println("*****REGISTRO******");
-				System.out.println("Introduzca los siguientes datos: ");
-				boolean numero=false;
-				String nombre = null;
-				do
-				{
-					try 
-					{
-						System.out.println("1.Nombre: ");
-						nombre=entrada.nextLine();
-						if(nombre.isEmpty())
-						{
-							nombre=entrada.nextLine();
-						}
-						verificarErrorNumeros(nombre);
-						numero=false;
-					} 
-					catch (ErrorIntroducirNumeros e) {
-						
-						System.out.println(e.getMessage());
-						numero=true;
-					}
-				}while(numero==true);
-					
+		boolean excepcion=false;
+		do
+		{
+			excepcion=false;
+			System.out.println("****MENU PRINCIPAL*****");
+			System.out.println("1.Ver catalogo");
+			System.out.println("2.Acceder");
+			System.out.println("3.Registrarse");
+			System.out.println("4.Salir");
+			System.out.println("Introduzca una opción: ");
+			try
+			{
+			opcion=entrada.nextInt();
 			
-				String apellidos= null;
-				do
+			switch(opcion)
+			{
+			case(1):					
+			    librosBiblio.mostrarCatalago();
+				mostrarDescripcionLibro();    
+				break;
+			case(2):	
+				accesoSocio();
+		        continuar2=true;
+				break;
+			case(3):
+				Usuario miusuario=new Usuario();
+
+			System.out.println("*****REGISTRO******");
+			System.out.println("Introduzca los siguientes datos: ");
+			boolean numero=false;
+			String nombre = null;
+			do
+			{
+				try 
 				{
-					try 
+					System.out.println("1.Nombre: ");
+					nombre=entrada.nextLine();
+					if(nombre.isEmpty())
 					{
-						System.out.println("2. Apellido: ");
-						apellidos=entrada.nextLine();
-						if(apellidos.isEmpty())
-						{
-							apellidos=entrada.nextLine();
-						}
-						verificarErrorNumeros(apellidos);
-						numero=false;
-					} 
-					catch (ErrorIntroducirNumeros e) {
-						
-						System.out.println(e.getMessage());
-						numero=true;
+						nombre=entrada.nextLine();
 					}
-				}while(numero==true);
-				
-				
-				System.out.println("3.Fecha de nacimiento (Escribalo de la forma: DD-MM-AA, incluyendo los guiones ");
-				String fecha=entrada.nextLine();
+					verificarErrorNumeros(nombre);
+					numero=false;
+				} 
+				catch (ErrorIntroducirNumeros e) {
+					
+					System.out.println(e.getMessage());
+					numero=true;
+				}
+			}while(numero==true);
+			
+			String apellidos= null;
+			do
+			{
+				try 
+				{
+					System.out.println("2. Apellido: ");
+					apellidos=entrada.nextLine();
+					if(apellidos.isEmpty())
+					{
+						apellidos=entrada.nextLine();
+					}
+					verificarErrorNumeros(apellidos);
+					numero=false;
+				} 
+				catch (ErrorIntroducirNumeros e) {
+					
+					System.out.println(e.getMessage());
+					numero=true;
+				}
+			}while(numero==true);
+			
+			System.out.println("3.Fecha de nacimiento (Escribalo de la forma: DD-MM-AAAA, incluyendo los guiones ");
+			
+			boolean correcto=false;
+			
+			String fecha;
+
+			do
+			{
+				correcto=true;
+				fecha=entrada.nextLine();
 				if(fecha.isEmpty())
 				{
-					fecha=entrada.nextLine();
+					fecha=entrada.nextLine();	
 				}
-				
-				System.out.println("4.Email");
+				if(formatoFechaCorrecto(fecha) ==true)
+				{
+					correcto=true;
+					
+				}
+				else
+				{
+					
+					System.out.println("Ha introducido datos erróneos, recuerde introducir la fecha de la forma DD-MM-AA, incluyendo los guiones, ingrese de nuevo su fecha de nacimiento: ");
+					correcto=false;	
+				}
+			
+			}while(correcto==false);
+			
+			
+			System.out.println("4.Email");
 
-				  int arroba=0;
-				  boolean repetido=false;
-				  String email;
-				  
-				  do
+			  int arroba=0;
+			  boolean repetido=false;
+			  String email;
+			  
+			  do
+				{
+					do
 					{
-						do
+						email=entrada.nextLine();
+						if(email.isEmpty())
 						{
 							email=entrada.nextLine();
-							if(email.isEmpty())
+						}
+						
+						if(mi_socio.Comprobar_email(email).equalsIgnoreCase(email))
+						{
+							System.out.println("Este email ya está registrado, ingrese otro email: ");
+							repetido=true;
+						}
+						else
+						{
+							repetido=false;
+							for (int i=0; i<email.length() ; i++)
 							{
-								email=entrada.nextLine();
+								if( email.charAt(i) == '@')
+								{
+									arroba++;
+									
+				                  System.out.println("5.Comprobar email:");
+									
+									boolean emailCorrecto=false;
+									
+									do
+									{
+										String comprobacionEmail=entrada.nextLine();
+										
+										if(comprobacionEmail.isEmpty())
+										{
+											comprobacionEmail=entrada.nextLine();
+										}
+										
+										if(!email.equalsIgnoreCase(comprobacionEmail))
+										{
+											System.out.println("El email no coincide, pruebe de nuevo");
+											emailCorrecto=false;
+										}
+										else
+										{
+											emailCorrecto=true;
+										}
+										
+									}while(emailCorrecto==false);
+								}	
+							}
+							if (arroba == 0)
+							{
+								System.out.println("El email suministrado no tiene @, por favor introduzca de nuevo el email:");
 							}
 							
-							if(mi_socio.Comprobar_email(email).equalsIgnoreCase(email))
-							{
-								System.out.println("Este email ya está registrado, ingrese otro email: ");
-								repetido=true;
-							}
-							else
-							{
-								repetido=false;
-								for (int i=0; i<email.length() ; i++)
-								{
-									if( email.charAt(i) == '@')
-									{
-										arroba++;
-										
-					                  System.out.println("5.Comprobar email:");
-										
-										boolean emailCorrecto=false;
-										
-										do
-										{
-											String comprobacionEmail=entrada.nextLine();
-											
-											if(comprobacionEmail.isEmpty())
-											{
-												comprobacionEmail=entrada.nextLine();
-											}
-											
-											if(!email.equalsIgnoreCase(comprobacionEmail))
-											{
-												System.out.println("El email no coincide, pruebe de nuevo");
-												emailCorrecto=false;
-											}
-											else
-											{
-												emailCorrecto=true;
-											}
-											
-										}while(emailCorrecto==false);
-									}	
-								}
-								if (arroba == 0)
-								{
-									System.out.println("El email suministrado no tiene @, por favor introduzca de nuevo el email:");
-								}
-								
-							}
-						}while(repetido==true);
-					}while(arroba==0);
-				
-				
-				System.out.println("6.Establezca una contraseña: ");
-				
-				boolean coincide=false;
-				
-				String contrasenya1;
-				
-				do
+						}
+					}while(repetido==true);
+				}while(arroba==0);
+			
+			
+			System.out.println("6.Establezca una contraseña: ");
+			
+			boolean coincide=false;
+			
+			String contrasenya1;
+			
+			do
+			{
+				contrasenya1=entrada.nextLine();
+				if(contrasenya1.isEmpty())
 				{
 					contrasenya1=entrada.nextLine();
-					if(contrasenya1.isEmpty())
-					{
-						contrasenya1=entrada.nextLine();
-					}
-					
-					System.out.println("7. Confirmar contraseña: ");
-					String contrasenya2=entrada.nextLine();
-					if(contrasenya2.isEmpty())
-					{
-						contrasenya2=entrada.nextLine();
-					}
-					
-					if(contrasenya1.equals(contrasenya2) ==false)
-					{
-						System.out.println("Las contraseñas no coinciden, establezca de nuevo la contraseña: ");
-						coincide=false;
-					}
-					else
-					{
-						System.out.println("Se ha registrado exitosamente");
-						coincide=true;
-					}
-				}while(coincide==false);
-				    mi_socio.registrarSocio(miusuario.Registrarse(nombre,apellidos,fecha,email,contrasenya1));
-				    continuar2=true;
-					break;
-				case(4):
-					System.out.println("Ha salido del programa");
-				    continuar2=false;
-					break;
-				default:
-					System.out.println("Opción errónea, introduzca una opción válida");
-					continuar2=true;
-					break;
 				}
-			}while(opcion <1 && opcion >4 || continuar2==true);
-			
-			} 
-			catch (InputMismatchException e) 
-			{		
-				//Quiero que cuando salte la excepción, diga esto y me aparezca el menu de nuevo
-				System.out.println("Ha introducido letras, intente de nuevo");
 				
+				System.out.println("7. Confirmar contraseña: ");
+				String contrasenya2=entrada.nextLine();
+				if(contrasenya2.isEmpty())
+				{
+					contrasenya2=entrada.nextLine();
+				}
+				
+				if(contrasenya1.equals(contrasenya2) ==false)
+				{
+					System.out.println("Las contraseñas no coinciden, establezca de nuevo la contraseña: ");
+					coincide=false;
+				}
+				else
+				{
+					System.out.println("Se ha registrado exitosamente");
+					coincide=true;
+				}
+			}while(coincide==false);
+			    mi_socio.registrarSocio(miusuario.Registrarse(nombre,apellidos,fecha,email,contrasenya1));
+
+			    continuar2=true;
+				break;
+			case(4):
+				System.out.println("Ha salido del programa");
+			    continuar2=false;
+				break;
+			default:
+				System.out.println("Opción errónea, introduzca una opción válida");
 			}
+			}catch(InputMismatchException e) 
+			{
+				System.err.println("Ha introducido letras,solo se permiten números, intente de nuevo ");//Mario
+				entrada.nextLine();
+				excepcion=true;
+			}
+		}while(opcion!=4 || continuar2==true|| excepcion==true);
 	
 				
 		
@@ -244,7 +260,7 @@ public class ClasePrincipal {
 		System.out.println("¿Quiere ver la descripción de algún libro? SI/NO");
 		 
 		 do
-		 {
+		 {	mostrar=false;
 			 String resp=entrada.nextLine();
 			    
 			    if(resp.isEmpty())
@@ -254,7 +270,7 @@ public class ClasePrincipal {
 	        
 			    do
 	         {
-	         	if(resp.equalsIgnoreCase("SI"))
+	         	if(resp.trim().equalsIgnoreCase("SI"))
 	 		    {
 
 	         			System.out.println("Escriba el titulo del libro:");
@@ -272,7 +288,10 @@ public class ClasePrincipal {
 	     		    	    	System.out.println(librosBiblio.descripcionLibro(titulo));
 	     		    	    	corresponde=false;
 	     		    	    	System.out.println("¿Quiere ver la descripción de otro libro? SI/NO");
-		        		    		
+		        		    	boolean corresponde1=false;
+	     		    	    	do
+	     		    	    	{
+	     		    	    		corresponde1=false;
 	     		    	    	String resp2=entrada.nextLine();
 			        		    	if(resp2.isEmpty())
 			        		    	{
@@ -288,6 +307,12 @@ public class ClasePrincipal {
 			        		        	
 	                                continuar=true;
 			        		    	}
+			        		        else
+			        		        {
+			        		        	System.out.println("Respuesta no permitida, recuerde ingresar SI o NO, intente de nuevo");
+			        		        	corresponde1=true;
+			        		        }
+		 		    	    	}while(corresponde1==true);
 		        		    		
 		        		    	}
 	     		    		else
@@ -300,7 +325,7 @@ public class ClasePrincipal {
 	     		    	
 	    
 	 		    }
-	 		    if(resp.equalsIgnoreCase("NO"))
+	 		    if(resp.trim().equalsIgnoreCase("NO"))
 	 		    {
 	 		    	continuar=false;
 	 		    	continuar2=true;
@@ -369,51 +394,67 @@ public class ClasePrincipal {
 	public static void menuSocio()
 	{
 		int opcion2=0;
-		
+		boolean excepcion=false;
 		do
 		{
+			excepcion=false;
 			System.out.println("*****MENÚ SOCIO******");
 		    System.out.println("1.Ver catalogo");
 		    System.out.println("2.Alquileres");
 		    System.out.println("3.Sanciones");
 		    System.out.println("4.Cerrar sesión");
+		    System.out.println("5.Salir");
 		     
 		    System.out.println("Introduzca una opcion:");
-		    opcion2=entrada.nextInt();
-	
-		    
-		    switch(opcion2)
+		    try
 		    {
-		    case(1):
-		    	librosBiblio.mostrarDisponibilidad();
-		        mostrarDescripcionLibroSocios();
-		    	break;
-		    case(2):
-		    	if(miPrestamo.numeroPrestamos(email) ==0)
-				{
-		        	System.out.println("*********** PRESTAMOS ***********\n");
-		        	System.out.println("No tiene prestamos en curso\n");
-				}
-		    	if(miPrestamo.numeroPrestamos(email)!=0)
-		    	{
-		    		miPrestamo.verPrestamos(email);
-			        devolverLibro();
-		    	}
-		    	break;
-		    case(3):
-		    	sancion.verSancion(email);
-		    	break;
-		    case(4):
-		    	menu();
-		    	//continuar2=true;
-		    	continuar3=false;  
-		    	break;
-		    default:
-		    	System.out.println("Opción errónea, intente de nuevo");
-		    	break;
+		    	 opcion2=entrada.nextInt();
+		    		
+				    
+				    switch(opcion2)
+				    {
+				    case(1):
+				    	librosBiblio.mostrarDisponibilidad();
+				        mostrarDescripcionLibroSocios();
+				    	break;
+				    case(2):
+				    	if(miPrestamo.numeroPrestamos(email) ==0)
+						{
+				        	System.out.println("*********** PRESTAMOS ***********\n");
+				        	System.out.println("No tiene prestamos en curso\n");
+						}
+				    	if(miPrestamo.numeroPrestamos(email)!=0)
+				    	{
+				    		miPrestamo.verPrestamos(email);
+					        devolverLibro();
+				    	}
+				    	break;
+				    case(3):
+				    	sancion.verSancion(email);
+				    	break;
+				    case(4):
+				    	System.out.println("Ha finalizado su sesión");
+				    	continuar3=false;  
+				    	break;
+				    case(5):
+				    	System.out.println("Ha salido del programa");
+				    //	continuar2=false; //Nunca lo hace se cierra..
+				    	break;
+				    default:
+				    	System.out.println("Opción errónea, intente de nuevo");
+				    	break;
+				    }
 		    }
-		}while(opcion2 <1 && opcion2>4 ||continuar3==true);
-	     
+		    catch(InputMismatchException e) 
+			{
+				System.err.println("Ha introducido letras,solo se permiten números, intente de nuevo ");
+				entrada.nextLine();
+				excepcion=true;
+			}
+		    
+		   
+		}while(opcion2 <1 && opcion2>5 ||continuar3==true|| excepcion==true );
+		
 	}
 
 	public static void mostrarDescripcionLibroSocios()
@@ -421,7 +462,7 @@ public class ClasePrincipal {
 		boolean mostrar=false; 
 		System.out.println("¿Quiere ver la descripción de algún libro? SI/NO");
 		do
-		{
+		{mostrar=false; 
 			String resp=entrada.nextLine();
 		    
 		    if(resp.isEmpty())
@@ -431,7 +472,7 @@ public class ClasePrincipal {
 	    
 		    do
 		    {
-	     	if(resp.equalsIgnoreCase("SI"))
+	     	if(resp.trim().equalsIgnoreCase("SI"))
 			    {
 
 	     			System.out.println("Escriba el titulo del libro:");
@@ -450,22 +491,30 @@ public class ClasePrincipal {
 	 		    	    	 alquilarLibro();	
 	 		    	    	corresponde=false;
 	 		    	    	System.out.println("¿Quiere ver la descripción de otro libro? SI/NO");
-	        		    		
-	 		    	    	String resp2=entrada.nextLine();
+	        		    	boolean corresponde1=false;
+	 		    	    	do
+	 		    	    	{corresponde1=false;
+	 		    	    		String resp2=entrada.nextLine();
 		        		    	if(resp2.isEmpty())
 		        		    	{
 		        		    		resp2=entrada.nextLine();
 		        		    	}
-		        		    	if(resp2.equalsIgnoreCase("NO"))
+		        		    	if(resp2.trim().equalsIgnoreCase("NO"))
 		        		    	{
 		        		    		continuar=false;
 		        		    		continuar2=true;
 		        		    	}
-		        		        if(resp2.equalsIgnoreCase("SI"))
+		        		        if(resp2.trim().equalsIgnoreCase("SI"))
 		        		    	{
-		        		       
-	                            continuar=true;
+		        		        	continuar=true;
 		        		    	}
+		        		        else
+		        		        {
+		        		        	System.out.println("Respuesta no permitida, recuerde ingresar SI o NO, intente de nuevo");
+		        		        	corresponde1=true;
+		        		        }
+	 		    	    	}while(corresponde1==true);
+	 		    	    	
 	        		    		
 	        		    	}
 	 		    		else
@@ -478,7 +527,7 @@ public class ClasePrincipal {
 	 		    	
 
 			    }
-			    if(resp.equalsIgnoreCase("NO"))
+			    if(resp.trim().equalsIgnoreCase("NO"))
 			    {
 			    	continuar=false;
 			    	continuar2=true;
@@ -608,19 +657,6 @@ public class ClasePrincipal {
 
 	}
 
-	public static void verificarErrorLetras(int opcion) throws ErrorIntroducirLetras
-	{
-		opcion=entrada.nextInt();
-		for(int i=0; i<Integer.toString(opcion).length() ; i++)
-		{
-			char numero=Integer.toString(opcion).charAt(i);
-			if(Character.isDigit(numero))
-			{
-				ErrorIntroducirLetras miExcepcion1=new ErrorIntroducirLetras("Ha introducido letras, intente de nuevo");
-				throw miExcepcion1;
-			}
-		}
-	}
 	
 	public static void verificarErrorNumeros(String nombre) throws ErrorIntroducirNumeros
 	{
@@ -634,4 +670,21 @@ public class ClasePrincipal {
 			}
 		}
 	}
+	
+	public static boolean formatoFechaCorrecto(String fecha) 
+	{
+		String DATE_FORMAT = "dd-MM-yyyy";
+	    try {
+	        DateFormat fechaFormato = new SimpleDateFormat(DATE_FORMAT);
+	        fechaFormato.setLenient(false);
+	        fechaFormato.parse(fecha); //Lanza una excepción
+	        return true;
+	    } 
+	    catch (ParseException e)
+	    {
+	        return false;
+	    }
+	}
+	
+	
 }
